6f318a80ebb57a0684f11130f7fcac3d6d9811e2
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-04-16 20:09:40 +0300

    lesson 3

--- main.py
+++ main.py
@@ -1,132 +1,92 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+from queue import Queue
 
 
-class Stack:
-    def __init__(self, max_size=None):
-        self.head = None
-        self.size = 0
-        self.max_size = max_size
+# queue = Queue()
+# queue.put(1)
+# queue.put(2)
+# queue.put(3)
+#
+# print(queue.get())
+# print(queue.get())
+# print(queue.get())
+# print(queue.empty())
 
-    def push(self, data):
-        if self.max_size and self.size == self.max_size:
-            raise OverflowError("Stack is full")
-        if self.max_size and self.size < self.max_size or self.max_size is None:
-            node = Node(data)
-            if self.head is None:
-                self.head = node
-            else:
-                node.next = self.head
-                self.head = node
-            self.size += 1
+class BankQueue:
+    def __init__(self):
+        self.queue = Queue()
 
-    def pop(self):
-        if self.size == 0:
-            raise IndexError("Stack is empty")
-        node = self.head
-        self.head = node.next
-        self.size -= 1
-        return node.data
+    def put_client(self, client):
+        self.queue.put(client)
 
-    def print(self):
-        if self.head is None:
-            raise IndexError("Stack is empty")
-        node = self.head
-
-        while node:
-            print(node.data, end="->")
-            node = node.next
-
-    def get_size(self):
-        return self.size
-
-    def set_max_size(self, new_size):
-        if new_size >= self.size:
-            self.max_size = new_size
+    def serve_next_client(self):
+        if self.queue.empty():
+            raise IndexError("No elements in queue")
         else:
-            raise IndexError("Stack size is bigger than max stack size")
+            client = self.queue.get()
+            print(f"Serve client {client}")
 
-    def get_max_size(self):
-        return self.max_size
+    def number_clients(self):
+        return self.queue.qsize()
 
-    def is_empty(self):
-        return self.size == 0
+    def is_empty_queue(self):
+        return self.queue.empty()
 
-    def is_full(self):
-        if self.max_size:
-            return self.size == self.max_size
-        else:
-            return self.size != 0
-
-    def clear(self):
-        self.head = None
-        self.size = 0
-
-    def peek(self):
-        if self.head is not None:
-            return self.head.data
-        else:
-            raise IndexError("Stack is empty")
+    def print_clients(self):
+        temp_queue = Queue()
+        while not self.queue.empty():
+            client = self.queue.get()
+            print(client, end=' <- ')
+            temp_queue.put(client)
+        print()
+        self.queue = temp_queue
 
 
-stack = Stack()
-try:
-    while True:
-        print("\nMenu:")
-        print("1. Set stack size")
-        print("2. Add new item to stack")
-        print("3. Remove item from stack")
-        print("4. Show stack")
-        print("5. Check if stack is empty")
-        print("6. Check if stack is full (for stack without fixed size will return 'TRUE' if stack has even one)"
-              "element")
-        print("7. Clear stack")
-        print("8. Get last stack element")
-        print("9. Exit")
+bank_queue = BankQueue()
+bank_queue.put_client("Mary")
+bank_queue.put_client("Kate")
+bank_queue.print_clients()
+bank_queue.serve_next_client()
+bank_queue.put_client("Nick")
+bank_queue.print_clients()
+bank_queue.serve_next_client()
+bank_queue.serve_next_client()
+print(bank_queue.is_empty_queue())
 
-        choice = input("Choose the option: ")
 
-        if choice == '1':
-            size = int(input("Enter stack size: "))
-            stack.set_max_size(size)
-        elif choice == '2':
-            item = input("Enter new stack item: ")
-            stack.push(item)
-            stack.print()
-        elif choice == '3':
-            print(stack.pop())
-        elif choice == '4':
-            stack.print()
-        elif choice == '5':
-            if stack.is_empty():
-                print("Stack is empty")
-            else:
-                print("Stack is not empty")
-        elif choice == '6':
-            if stack.get_max_size():
-                if stack.is_full():
-                    print("Stack is full")
-                else:
-                    print("Stack isn't full")
-            else:
-                if stack.is_full():
-                    print("Stack has even one element")
-                else:
-                    print("Stack is empty")
-        elif choice == '7':
-            stack.clear()
-        elif choice == '8':
-            print("Last stack element: ", stack.peek())
-        elif choice == '9':
-            print("Bye!")
-            break
-        else:
-            print("Unknown operation!")
-except IndexError as e:
-    print("Message: ", e)
-except OverflowError as e:
-    print("Message: ", e)
-except Exception as e:
-    print("Message: ", e)
+#Tasts
+from queue import PriorityQueue
+# queue = PriorityQueue()
+# queue.put((1, "John"))
+# queue.put((2, "Max"))
+# queue.put((3, "Kate"))
+# queue.put((2, "Sonya"))
+# queue.put((-1, "John1"))
+# queue.put((-3, "Kate1"))
+# queue.put((-2, "Sonya1"))
+
+# while not queue.empty():
+#     priority, client = queue.get()
+#     print(client, priority)
+
+class TaskSolver:
+    def __init__(self):
+        self.queue = PriorityQueue()
+
+    def add_task(self, priority, task):
+        self.queue.put((priority, task))
+
+    def solve_next_task(self):
+        if self.queue.empty():
+            print("No tasks")
+            return
+
+        priority, task = self.queue.get()
+        print(f"Solve task {task}")
+
+solver = TaskSolver()
+solver.add_task(2, "Task1")
+solver.add_task(1, "Task2")
+solver.add_task(3, "Task3")
+solver.solve_next_task()
+solver.solve_next_task()
+solver.solve_next_task()
