744fed0109bad3d14406ad4cdfd2380068ba4221
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-05-01 19:49:17 +0300

    lesson 7 hw

--- main.py
+++ main.py
@@ -1,61 +1,67 @@
 import threading
+import random
 import pickle
+from datetime import datetime
+import os
+import glob
+import re
+
+# task1
+
+size = 100
+numbers = []
 
 
-#task1 - task2
-def max_number(arr):
-    if not arr:
-        raise IndexError("List is empty")
-    print(f"Max number: {max(arr)}")
+def random_numbers():
+    global numbers
+
+    numbers = [random.randint(1, 999) for _ in range(size)]
+    print(f"Random numbers list: {numbers}")
 
 
-def min_number(arr):
-    if not arr:
-        raise IndexError("List is empty")
-    print(f"Min number: {min(arr)}")
+def suma():
+    try:
+        if not numbers:
+            raise IndexError("List is empty")
+        print(f"Sum of numbers: {sum(numbers)}")
+    except IndexError as e:
+        print("Message: ", e)
 
 
-def suma(arr):
-    if not arr:
-        raise IndexError("List is empty")
-    print(f"Sum of numbers: {sum(arr)}")
+def mean():
+    try:
+        if not numbers:
+            raise IndexError("List is empty")
+        mean_value = sum(numbers) / size
+        print(f"Mean of numbers: {round(mean_value, 2)}")
+    except IndexError as e:
+        print("Message: ", e)
 
 
-def mean(arr):
-    if not arr:
-        raise IndexError("List is empty")
-    size = len(arr)
-    mean_value = sum(arr) / size
-    print(f"Mean of numbers: {round(mean_value, 2)}")
+t1 = threading.Thread(target=random_numbers, args=())
+t2 = threading.Thread(target=suma, args=())
+t3 = threading.Thread(target=mean, args=())
+
+t1.start()
+t1.join()
+
+t2.start()
+t3.start()
+
+t2.join()
+t3.join()
+
+# task2
+numbers_file = "numbers.pickle"
+prime_numbers_file = "prime_numbers.pickle"
+numbers_factorial_file = "numbers_factorial.pickle"
 
 
-try:
-    numbers = list(map(int, input("Enter numbers divided by spaces: ").split()))
-    print(numbers)
-    t_max = threading.Thread(target=max_number, args=(numbers,))
-    t_min = threading.Thread(target=min_number, args=(numbers,))
-    t_sum = threading.Thread(target=suma, args=(numbers,))
-    t_mean = threading.Thread(target=mean, args=(numbers,))
-
-    t_max.start()
-    t_min.start()
-    t_sum.start()
-    t_mean.start()
-
-    t_max.join()
-    t_min.join()
-    t_sum.join()
-    t_mean.join()
-
-except Exception as e:
-    print("Message: ", e)
-
-
-
-# task3
-file_numbers = "numbers.pickle"
-file_even_numbers = "even_numbers.pickle"
-file_odd_numbers = "odd_numbers.pickle"
+def save_numbers():
+    print("\nStart saving numbers: ", datetime.now())
+    numbers = [random.randint(1, 10) for _ in range(size)]
+    write_numbers(numbers, numbers_file)
+    print("\nSaving numbers ends: ", datetime.now())
 
 
 def write_numbers(numbers, file):
@@ -72,39 +78,94 @@ def read_numbers(file):
     return read_num
 
 
-def even_numbers():
-    read_num = read_numbers(file_numbers)
-    even_num = []
-    for num in read_num:
-        if num % 2 == 0:
-            even_num.append(num)
-    write_numbers(even_num, file_even_numbers)
-    read_num = read_numbers(file_even_numbers)
-    print(read_num)
+def is_prime():
+    print("\nStart searching for prime numbers numbers: ", datetime.now())
+    prime_num = []
+    read_num = read_numbers(numbers_file)
+    print("Origin list: ", read_num)
+    for item in read_num:
+        if item <= 1:
+            continue
+        prime = True
+        for i in range(2, int(item ** 0.5) + 1):
+            if item % i == 0:
+                prime = False
+                break
+        if prime:
+            prime_num.append(item)
+    write_numbers(prime_num, prime_numbers_file)
+    print("Prime numbers: ", read_numbers(prime_numbers_file))
+    print("\nEnd searching for prime numbers numbers: ", datetime.now())
 
 
-def odd_numbers():
-    read_num = read_numbers(file_numbers)
-    odd_num = []
-    for num in read_num:
-        if num % 2 != 0:
-            odd_num.append(num)
-    write_numbers(odd_num, file_odd_numbers)
-    read_num = read_numbers(file_odd_numbers)
-    print(read_num)
+def factorial_save():
+    print("\nStart calculating factorial: ", datetime.now())
+    num_factorial = []
+    read_num = read_numbers(numbers_file)
+    print("Origin list: ", read_num)
+    for item in read_num:
+        num_factorial.append(factorial(item))
+    write_numbers(num_factorial, numbers_factorial_file)
+    print("Factorials: ", read_numbers(numbers_factorial_file))
+    print("\nEnd calculating factorial: ", datetime.now())
 
 
-try:
-    numbers = list(map(int, input("Enter numbers divided by spaces: ").split()))
-    write_numbers(numbers, file_numbers)
-    t_even = threading.Thread(target=even_numbers, args=())
-    t_odd = threading.Thread(target=odd_numbers, args=())
+def factorial(number):
+    if number == 0:
+        return 1
+    else:
+        return number * factorial(number - 1)
 
-    t_even.start()
-    t_odd.start()
 
-    t_even.join()
-    t_odd.join()
+t1 = threading.Thread(target=save_numbers, args=())
+t2 = threading.Thread(target=is_prime, args=())
+t3 = threading.Thread(target=factorial_save, args=())
 
-except Exception as e:
-    print("Message: ", e)
+t1.start()
+t1.join()
+
+t2.start()
+t3.start()
+
+t2.join()
+t3.join()
+
+# task3
+dir_path = "hw_files"
+concatenate_file = "concatenate_file.txt"
+forbidden_words_file = "forbidden_words.txt"
+files = glob.glob(os.path.join(dir_path, '*'))
+
+
+def concatenate_files():
+    print("\nStart concatenate file: ", datetime.now())
+    with open(concatenate_file, 'w') as result_file:
+        for file in files:
+            with open(file, 'r') as f:
+                file_content = f.read()
+                result_file.write(file_content + "\n")
+    print("\nEnd concatenate file: ", datetime.now())
+
+
+def remove_words():
+    print("\nStart remove words: ", datetime.now())
+    with open(forbidden_words_file, 'r') as forbidden_file:
+        forbidden_words = list(forbidden_file.read().split())
+    with open(concatenate_file, 'r') as result_file:
+        content = result_file.read()
+    for word in forbidden_words:
+        if word in content:
+            content = re.sub(r'\b{}\b'.format(re.escape(word)), '', content)
+    with open(concatenate_file, 'w') as result_file:
+        result_file.write(content)
+    print("\nEnd remove words: ", datetime.now())
+
+
+t1 = threading.Thread(target=concatenate_files, args=())
+t2 = threading.Thread(target=remove_words, args=())
+
+t1.start()
+t1.join()
+
+t2.start()
+t2.join()
